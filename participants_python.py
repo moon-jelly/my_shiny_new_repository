4f3a02c82acbd9f7057c3c0c9913ccd4334c51d8
filterfeeder <martha.elke01@gmail.com>
2016-09-02 13:23:24 +1200

    added info for participants. Made new function for numner 2 addition

--- participants_python.py
+++ participants_python.py
@@ -1,3 +1,7 @@
 #I would like to write some Python code here that adds two numbers but I don't know how to do that
+def add2(x,y):
+	return x+y
+
 #I would like to write some code that prints out the name of each participant of this workshop
+print("Martha")
 #Can you help me using a Pull Requests?
